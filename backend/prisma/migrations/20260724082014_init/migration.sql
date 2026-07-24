f7bf842639a9185bbbe2e6481bb54835d611cd47
Rohit <rrohit2911@gmail.com>
2026-07-24 16:13:32 +0530

    Add RDS database

---
+++ backend/prisma/migrations/20260724082014_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE "documents" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "file_url" TEXT NOT NULL,
+    "original_filename" TEXT,
+    "expiry_date" DATE,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reminders" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "document_id" BIGINT NOT NULL,
+    "remind_at" TIMESTAMP(6) NOT NULL,
+    "status" VARCHAR(50) DEFAULT 'pending',
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "reminders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" BIGSERIAL NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "documents" ADD CONSTRAINT "fk_user" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "reminders" ADD CONSTRAINT "fk_document_reminder" FOREIGN KEY ("document_id") REFERENCES "documents"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "reminders" ADD CONSTRAINT "fk_user_reminder" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
